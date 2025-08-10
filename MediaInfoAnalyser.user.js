405402066fd61a166d57796912c902546e8e81d3
Horo <225634745+horo-rto@users.noreply.github.com>
2025-08-10 06:19:26 +0400

    Create MediaInfoAnalyser.user.js

---
+++ MediaInfoAnalyser.user.js
@@ -0,0 +1,360 @@
+// ==UserScript==
+// @name         RTO MediaInfo analyser
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  MediaInfo analyser!
+// @author       Horo
+// @updateURL    https://raw.githubusercontent.com/horo-rto/TampermonkeyRtoScripts/refs/heads/main/MediaInfoAnalyser.user.js
+// @match        https://rutracker.org/forum/viewtopic.php?t=*
+// @match        https://rutracker.net/forum/viewtopic.php?t=*
+// @match        https://rutracker.nl/forum/viewtopic.php?t=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=rutracker.org
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
+
+var post;
+var regex1 = /<div class="sp-wrap">.*?<div class="sp-body">.*?<\/div>\n<\/div>/gms;
+var video_size;
+var video_bitrate;
+var is_displayed;
+
+class General {
+    constructor() { }
+
+    size = -1;
+    bitrate = -1;
+
+    toString() {
+        return "        " + this.size + " " + this.bitrate;
+    }
+}
+class Video {
+    constructor() { }
+
+    codec = "";
+    hdr = "";
+    height = -1;
+    width = -1;
+    fps = -1;
+    vfr = -1;
+    bitrate = -1;
+    bit = -1;
+    size = -1;
+    percentage = -1;
+    language = -1;
+    default = -1;
+    forced = -1;
+
+    toString() {
+        var line = "";
+        line += (this.default == 1 ? "[x]" : "<span style=\"color: red; font-weight: bold;\">[?]</span>" );
+        line += (this.forced == 1 ? "<span style=\"color: red; font-weight: bold;\">[x]</span>" : "[ ]" );
+
+        if (this.percentage < 50)
+            line += "<span style=\"color: red; font-weight: bold;\">[" + this.percentage + "%]</span>";
+        else
+            line += "[" + this.percentage + "%]";
+
+        line += " " + this.codec + " " + this.width + "x" + this.height + " ";
+        line += this.fps + "fps ";
+        if (this.vfr == 1) line += "(VFR) ";
+        line += this.bitrate + " " + this.bit + "bit";
+        if (this.hdr != "") line += "<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + this.hdr;
+        return line;
+    }
+}
+class Audio {
+    constructor() { }
+
+    title = "";
+    codec = "";
+    channels = "";
+    bitrate = -1;
+    samplingrate = -1;
+    size = -1;
+    percentage = -1;
+    language = -1;
+    default = -1;
+    forced = -1;
+    isfirst = -1;
+
+    toString() {
+        var line = "";
+        if (this.default == 1 && this.isfirst != 1){
+            line += (this.default == 1 ? "<span style=\"color: red; font-weight: bold;\">[x]</span>" : "[ ]" );
+        }else{
+            line += (this.default == 1 ? "[x]" : "[ ]" );
+        }
+        line += (this.forced == 1 ? "<span style=\"color: red; font-weight: bold;\">[x]</span>" : "[ ]" );
+
+        if (video_size < this.size*3)
+            var sizeError = true;
+
+        if (sizeError){
+            if (this.percentage < 10)
+                line += "<span style=\"color: red; font-weight: bold;\">[0" + this.percentage + "%]</span> ";
+            else
+                line += "<span style=\"color: red; font-weight: bold;\">[" + this.percentage + "%]</span> ";
+        }else{
+            if (this.percentage < 10)
+                line += "[0" + this.percentage + "%] ";
+            else
+                line += "[" + this.percentage + "%] ";
+        }
+
+        line += " " + this.codec + " " + this.channels + "ch ";
+
+        if (sizeError)
+            line += "<span style=\"color: red; font-weight: bold;\">" + this.bitrate + "</span> ";
+        else
+            line += this.bitrate + " ";
+        line += this.samplingrate + "kHz " + this.language + " " + this.title;
+        return line;
+    }
+}
+class Text {
+    constructor() { }
+
+    title = "";
+    codec = "";
+    count = -1;
+    language = -1;
+    default = -1;
+    forced = -1;
+    isfirst = -1;
+
+    toString() {
+        var line = "";
+        if (this.default == 1 && this.isfirst != 1){
+            line += (this.default == 1 ? "<span style=\"color: red; font-weight: bold;\">[x]</span>" : "[ ]" );
+        }else{
+            line += (this.default == 1 ? "[x]" : "[ ]" );
+        }
+        line += (this.forced == 1 ? "<span style=\"color: red; font-weight: bold;\">[x]</span>" : "[ ]" );
+        line += " " + this.codec + " " + this.language + ", " + this.count + " lines, " + this.title;
+        return line;
+    }
+}
+
+(function() {
+    console.log("RTO mediainfo analyser")
+
+    if(window.location.href.match(/start=\d+/) != null)
+        return;
+
+    is_displayed = GM_getValue("mi_box_displayed") ?? true;
+
+    let posts = $('#topic_main > tbody > tr > .td2 > .post_wrap > .post_body');
+    post = posts[0];
+
+    var spoiler = "";
+    var a = post.innerHTML.match(regex1);
+    for (const element of a) {
+        if (element.includes("Frame rate") || element.includes("Частота кадров"))
+            spoiler = element;
+    }
+
+    var mi = spoiler.match(/<div class="sp-body">.*?<\/div>\n<\/div>/gms)[0];
+    mi = mi.slice(22, mi.length-13);
+    mi = mi.replace("<pre class=\"post-pre\">", "").replace("</pre>", "");
+
+    var genrl = null;
+    var video = null;
+    var audio = [];
+    var subtl = [];
+
+    var chunks = mi.split("<span class=\"post-br\"><br></span>");
+
+    for (const chunk of chunks) {
+        if (chunk.includes("General") || chunk.includes("Общее")){
+            genrl = parce_general(chunk);
+        } else if (chunk.includes("Text") || chunk.includes("Текст")){
+            subtl.push(parce_text(chunk));
+        } else if (chunk.includes("Audio") || chunk.includes("Аудио")){
+            audio.push(parce_audio(chunk));
+        } else if (chunk.includes("Video") || chunk.includes("Видео")){
+            video = parce_video(chunk);
+        }
+    }
+
+    var box = $('<div>', {id: 'mi_box', style:
+                          "position: fixed; bottom:20%; right: -5px; padding: 10px 10px 10px 15px; " +
+                          "background-color: #dee3e7; border-radius: 5px; border: 1px solid #80808080;" +
+                          "font-family: \"Lucida Console\", Consolas, monospace; font-size: 12px; line-height: 14px;"});
+    $('body').append(box);
+    var slider = $('<div>', {id: 'mi_box_slider', style:
+                          "position: absolute; top:0px; left: 0px; width: 12px; height: 100%;" +
+                          " border-right: 1px solid #80808080; cursor: pointer;"});
+    $('#mi_box').append(slider);
+    var arrow_right = $('<div>', {id: 'mi_box_arrow_right', style: "position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: gray;"});
+    var arrow_left = $('<div>', {id: 'mi_box_arrow_left', style: "position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: gray;"});
+    $('#mi_box_slider').append(arrow_right);
+    $('#mi_box_slider').append(arrow_left);
+    $('#mi_box_slider')[0].addEventListener ("click", toggle , false);
+    $('#mi_box_arrow_right').append("⯈");
+    $('#mi_box_arrow_left').append("⯇");
+
+    $('#mi_box_arrow_right')[0].style.display = is_displayed ? "block" : "none";
+    $('#mi_box_arrow_left')[0].style.display = is_displayed ? "none" : "block";
+    if (!is_displayed)
+        $('#mi_box')[0].style.transform = "translate(calc(100% - 20px), 0)";
+
+    console.log(video);
+    console.log(audio);
+
+    var text = "";
+    text = video.toString() + "<hr>";
+    audio.forEach((stream) => text += stream.toString() + "<br>");
+    text += "<hr>";
+    subtl.forEach((stream) => text += stream.toString() + "<br>");
+
+    $('#mi_box').append(text);
+})();
+
+function toggle() {
+    console.log("is_displayed: " + is_displayed);
+    is_displayed = !is_displayed;
+    GM_setValue("mi_box_displayed", is_displayed);
+
+    if(is_displayed)
+        $('#mi_box')[0].style.transform = "translate(0, 0)";
+    else
+        $('#mi_box')[0].style.transform = "translate(calc(100% - 20px), 0)";
+
+    $('#mi_box_arrow_right')[0].style.display = is_displayed ? "block" : "none";
+    $('#mi_box_arrow_left')[0].style.display = is_displayed ? "none" : "block";
+}
+
+function parce_general(chunk){
+    var parced = new General();
+
+    var lines = chunk.replaceAll('&nbsp;', ' ').split("<br>");
+    for (const line of lines) {
+        if (line.includes("File size") || line.includes("Размер файла")){
+            parced.size = line.split(" : ")[1];
+        }else if (line.includes("Overall bit rate") || line.includes("Общий битрейт")){
+            parced.bitrate = line.split(" : ")[1];
+        }
+    }
+    return parced;
+}
+function parce_video(chunk){
+    var parced = new Video();
+
+    var lines = chunk.replaceAll('&nbsp;', ' ').split("<br>");
+    for (const line of lines) {
+        if ((line.startsWith("Format ") && !line.includes("Format profile") && !line.includes("Format settings")) || line.startsWith("Формат ")){
+            parced.codec = line.split(" : ")[1];
+        }else if (line.includes("HDR")){
+            parced.hdr = line.split(" : ")[1];
+        }else if (line.includes("Height") || line.includes("Высота")){
+            parced.height = line.split(" : ")[1].replaceAll(/\D/g, '');
+        }else if (line.includes("Width") || line.includes("Ширина")){
+            parced.width = line.split(" : ")[1].replaceAll(/\D/g, '');
+        }else if (line.includes("Frame rate mode") || line.includes("Режим частоты кадров")){
+            if (line.includes("Variable") || line.includes("Переменный"))
+                parced.vfr = 1;
+        }else if (line.includes("Frame rate") || (line.includes("Частота кадров") && !line.includes("Частота кадров в оригинале"))){
+            parced.fps = line.split(" : ")[1].split(" ")[0];
+        }else if (line.includes("Bit rate") || line.includes("Битрейт")){
+            parced.bitrate = line.split(" : ")[1].replaceAll(/ /g, '').replaceAll("Кбит/сек","kbps").replaceAll("kb/s","kbps").replaceAll("Мбит/сек","Mbps").replaceAll("Mb/s","Mbps");
+            video_bitrate = parced.bitrate;
+        }else if (line.includes("Bit depth") || line.includes("Битовая глубина")){
+            parced.bit = line.split(" : ")[1].replaceAll(/\D/g, '');
+        }else if (line.includes("Stream size") || line.includes("Размер потока")){
+            var newline = line.split(" : ")[1].split("(");
+            var size = newline[0].replaceAll(/[a-zA-Zа-яА-Я ]/g, '');
+            var value = newline[0].replaceAll(/[0-9,\. ]/g, '');
+            if (value == "Гбайт" || value == "GiB" )
+                parced.size = size.replace(",", ".")*1024;
+            else
+                parced.size = size;
+            parced.percentage = newline[1].slice(0,-2);
+            video_size = parced.size;
+        }else if (line.includes("Language") || line.includes("Язык")){
+            parced.language = line.split(" : ")[1];
+        }else if (line.includes("Default") || line.includes("По умолчанию")){
+            if (line.split(" : ")[1] == "Да" || line.split(" : ")[1] == "Yes")
+                parced.default = 1;
+            else
+                parced.default = 0;
+        }else if (line.includes("Forced") || line.includes("Принудительно")){
+            if (line.split(" : ")[1] == "Да" || line.split(" : ")[1] == "Yes")
+                parced.forced = 1;
+            else
+                parced.forced = 0;
+        }
+    }
+    return parced;
+}
+function parce_audio(chunk){
+    var parced = new Audio();
+
+    var lines = chunk.replaceAll('&nbsp;', ' ').split("<br>");
+    for (const line of lines) {
+        if (line == "Audio #1" || line == "Аудио #1" || line == "Audio" || line == "Аудио"){
+            parced.isfirst = 1;
+        }else if (line.startsWith("Title") || line.startsWith("Заголовок")){
+            parced.title = line.split(" : ")[1];
+        }else if (line.startsWith("Format ") || line.startsWith("Формат ")){
+            parced.codec = line.split(" : ")[1];
+        }else if (line.includes("Channel(s)") || line.includes("Канал(-ы)")){
+            parced.channels = line.split(" : ")[1].split(" ")[0];
+        }else if (line.includes("Bit rate") || line.includes("Битрейт")){
+            parced.bitrate = line.split(" : ")[1].replaceAll(/ /g, '').replaceAll("Кбит/сек","kbps").replaceAll("kb/s","kbps").replaceAll("Мбит/сек","Mbps").replaceAll("Mb/s","Mbps");
+        }else if (line.includes("Sampling rate") || line.includes("Частота дискретизации")){
+            parced.samplingrate = line.split(" : ")[1].split(" ")[0];
+        }else if (line.includes("Stream size") || line.includes("Размер потока")){
+            var size = line.split(" : ")[1].split(" ");
+            if (size[1] == "Гбайт" || size[1] == "GiB" )
+                parced.size = size[0].replace(",", ".")*1024;
+            else
+                parced.size = size[0];
+            parced.percentage = size[2].slice(1,-2);
+        }else if (line.includes("Language") || line.includes("Язык")){
+            parced.language = line.split(" : ")[1];
+        }else if (line.includes("Default") || line.includes("По умолчанию")){
+            if (line.split(" : ")[1] == "Да" || line.split(" : ")[1] == "Yes")
+                parced.default = 1;
+            else
+                parced.default = 0;
+        }else if (line.includes("Forced") || line.includes("Принудительно")){
+            if (line.split(" : ")[1] == "Да" || line.split(" : ")[1] == "Yes")
+                parced.forced = 1;
+            else
+                parced.forced = 0;
+        }
+    }
+    return parced;
+}
+function parce_text(chunk){
+    var parced = new Text();
+
+    var lines = chunk.replaceAll('&nbsp;', ' ').split("<br>");
+    for (const line of lines) {
+        if (line == "Text #1" || line == "Текст #1" || line == "Text" || line == "Текст"){
+            parced.isfirst = 1;
+        }else if (line.startsWith("Title") || line.startsWith("Заголовок")){
+            parced.title = line.split(" : ")[1];
+        }else if (line.startsWith("Format ") || line.startsWith("Формат ")){
+            parced.codec = line.split(" : ")[1];
+        }else if (line.includes("Count of elements") || line.includes("Число элементов")){
+            parced.count = line.split(" : ")[1];
+        }else if (line.includes("Language") || line.includes("Язык")){
+            parced.language = line.split(" : ")[1];
+        }else if (line.includes("Default") || line.includes("По умолчанию")){
+            if (line.split(" : ")[1] == "Да" || line.split(" : ")[1] == "Yes")
+                parced.default = 1;
+            else
+                parced.default = 0;
+        }else if (line.includes("Forced") || line.includes("Принудительно")){
+            if (line.split(" : ")[1] == "Да" || line.split(" : ")[1] == "Yes")
+                parced.forced = 1;
+            else
+                parced.forced = 0;
+        }
+    }
+    return parced;
+}
+
