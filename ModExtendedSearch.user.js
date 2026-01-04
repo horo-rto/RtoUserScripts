057c75d13d38a9b4972049fbbe538787d3db539f
Horo <225634745+horo-rto@users.noreply.github.com>
2026-01-04 20:43:17 +0400

    Update ModExtendedSearch.user.js - header reattempt

--- ModExtendedSearch.user.js
+++ ModExtendedSearch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         RTO mod extended search
+// @name         RTO title database search
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  extended anime mod links!
 // @author       Horo
 // @updateURL    https://raw.githubusercontent.com/horo-rto/RtoUserscripts/refs/heads/main/ModExtendedSearch.user.js
@@ -14,29 +14,37 @@
 // ==/UserScript==
 
 (function() {
+    console.log("RTO title database search")
+    init()
 
-console.log("RTO mod extended search")
+    function create_button(name, url, text) {
+        var button = $("<li>").append(name);
+        var replacedUrl = url.replace('%s', text);
+        button.wrapInner("<a href='"+replacedUrl+"'>");
+        return button[0];
+    }
 
-var tags = $('#searchTagItems > ul > li');
+    function init() {
+        // Находим контейнер с кнопкой поиска
+        var tags = $('#searchTagItems > ul > li');
+        console.log(tags);
+        if (!tags) {
+            setTimeout(init, 100);
+            return;
+        }
 
-function create_button(name, url, text) {
-    var button = $("<li>").append(name);
-    var replacedUrl = url.replace('%s', text);
-    button.wrapInner("<a href='"+replacedUrl+"'>");
-    return button[0];
-}
+        for (var i = 0; i < tags.length; i++) {
+            var text = tags[i].innerText.split(" ⇓")[0];
+            var ul = tags[i].lastChild;
 
-for (var i = 0; i < tags.length; i++) {
-    var text = tags[i].innerText.split(" ⇓")[0];
-    var ul = tags[i].lastChild;
-
-    ul.appendChild(create_button("<hr>", "", text));
-    ul.appendChild(create_button("По разделу", "https://rutracker.org/forum/tracker.php?f=1105,1106,1386,1387,1389,1390,1391,1642,2484,2491,2544,404,599,809,893&nm=%s", text));
-    ul.appendChild(create_button("world-art", "http://www.world-art.ru/search.php?name=%s&global_sector=animation", text));
-    ul.appendChild(create_button("shikimori", "https://shikimori.one/animes?search=%s", text));
-    ul.appendChild(create_button("aniDB", "https://anidb.net/search/anime/?adb.search=%s", text));
-    ul.appendChild(create_button("MAL", "https://myanimelist.net/anime.php?cat=anime&q=%s", text));
-    ul.appendChild(create_button("nyaa.si", "https://nyaa.si/?f=0&c=1_0&q=%s", text));
-}
+            ul.appendChild(create_button("<hr>", "", text));
+            ul.appendChild(create_button("По разделу", "https://rutracker.org/forum/tracker.php?f=1105,1106,1386,1387,1389,1390,1391,1642,2484,2491,2544,404,599,809,893&nm=%s", text));
+            ul.appendChild(create_button("world-art", "http://www.world-art.ru/search.php?name=%s&global_sector=animation", text));
+            ul.appendChild(create_button("shikimori", "https://shikimori.one/animes?search=%s", text));
+            ul.appendChild(create_button("aniDB", "https://anidb.net/search/anime/?adb.search=%s", text));
+            ul.appendChild(create_button("MAL", "https://myanimelist.net/anime.php?cat=anime&q=%s", text));
+            ul.appendChild(create_button("nyaa.si", "https://nyaa.si/?f=0&c=1_0&q=%s", text));
+        }
+    }
 
 })();
